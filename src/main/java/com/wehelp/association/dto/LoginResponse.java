f0f74f6203aa01998695237467aa7865fdb6bdc0
Abdellatif HAGROUF <abdellatif.hagrouf@gmail.com>
2025-05-21 23:26:34 +0200

    Added role to Login response

--- src/main/java/com/wehelp/association/dto/LoginResponse.java
+++ src/main/java/com/wehelp/association/dto/LoginResponse.java
@@ -7,10 +7,12 @@ public class LoginResponse {
     private int code;
     private String message;
     private String token;
+    private String role;
 
-    public LoginResponse(int code, String message, String token) {
+    public LoginResponse(int code, String message, String token, String role) {
         this.code = code;
         this.message = message;
         this.token = token;
+        this.role = role;
     }
 }
